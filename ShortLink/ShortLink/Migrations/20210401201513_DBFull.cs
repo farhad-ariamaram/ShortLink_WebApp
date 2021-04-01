9b0c4e2633f85e772b943f886e04cb13296f82cb
Farhad Ariamaram <farhad.ariamaram@gmail.com>
2021-04-02 00:50:24 +0430

    add migration

---
+++ ShortLink/ShortLink/Migrations/20210401201513_DBFull.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShortLink.Migrations
+{
+    public partial class DBFull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Page",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShortKey = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Page", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Page");
+        }
+    }
+}
