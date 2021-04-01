237a74d527b80a479f7bcb7e10f2d7bd072b945c
Farhad Ariamaram <farhad.ariamaram@gmail.com>
2021-04-01 23:57:52 +0430

    backend without validation

--- ShortLink/ShortLink/Pages/Index.cshtml.cs
+++ ShortLink/ShortLink/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using ShortLink.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,51 @@ namespace ShortLink.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ShortLinkDBContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ShortLinkDBContext db)
         {
-            _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
 
+        }
+
+        [BindProperty]
+        public Models.Page page { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Models.Page p = new Models.Page()
+            {
+                CreatedDate = DateTime.Now,
+                ShortKey = CreateNewShortLink(),
+                Link = page.Link
+            };
+
+            _db.Pages.Add(p);
+            _db.SaveChanges();
+
+            return RedirectToPage("View",new { id=p.ShortKey });
+        }
+
+        private string CreateNewShortLink()
+        {
+            string key = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 6);
+
+            while (_db.Pages.Any(s => s.ShortKey == key))
+            {
+                key = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 6);
+            }
+
+            return key;
         }
     }
 }
--- ShortLink/ShortLink/Pages/View.cshtml.cs
+++ ShortLink/ShortLink/Pages/View.cshtml.cs
@@ -4,13 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShortLink.Models;
 
 namespace ShortLink.Pages
 {
     public class ViewModel : PageModel
     {
-        public void OnGet()
+        private readonly ShortLinkDBContext _db;
+
+        public ViewModel(ShortLinkDBContext db)
         {
+            _db = db;
         }
+
+        public Models.Page page { get; set; }
+
+        public IActionResult OnGet(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return RedirectToPage("Error");
+            }
+
+            page = _db.Pages.FirstOrDefault(a => a.ShortKey.Equals(id));
+
+            if (page == null)
+            {
+                return RedirectToPage("Error");
+            }
+
+            return Page();
+        }
+
+
+
     }
 }
